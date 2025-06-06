a874332cafd18836549c1e9cda643258101eacdd
Gabriel Balceda <gabrielbalceda@gmail.com>
2025-06-06 20:11:44 -0300

    Se agrego el ejercicio numero tres de sumas

---
+++ JS/03-suma.js
@@ -0,0 +1,5 @@
+//Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+
+let suma = 3+5;
+
+document.writeln('<h1>El resultado de la suma de 3 + 5 es: '+suma+'</h1>')
